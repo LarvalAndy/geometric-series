38881e0bf5794a2934cd2f7ca6700170d9a77d3a
larvalandy <andyb3333@protonmail.com>
2022-03-01 21:58:02 +0900

    added input form and input evemt listener. TODO: add circles ased on input

--- geometricSeriesSum.js
+++ geometricSeriesSum.js
@@ -1,13 +1,22 @@
 let baseNumber = 1;
 let friendsNumber = 6;
-let layers = 1;
+let layers = 3;
 let sum = 0;
+
+//gives friendsInput to friendsNumber
+document.getElementById("friendsInput").addEventListener("change", e => {friendsNumber = e.target.value});
 
 function GeometricSeriesSum (a,r,n) { 
 	sum = ( a * (Math.pow(r,n) - 1 ))/(r - 1);
 	//not including the initial person
 	sum --;
 	return sum;
+}
+function AddCircles (friendsNum, layers) {
+	console.log(friendsNum);
+}
+function onNewNum () {
+
 }
 GeometricSeriesSum(baseNumber,friendsNumber,layers);
 document.getElementById("friendsTextResult").innerHTML = `You have ${sum} friends.`;
