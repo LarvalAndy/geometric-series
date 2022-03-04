6c42bb0f21d222cc5b7c6af210067e338fc79f42
larvalandy <andyb3333@protonmail.com>
2022-03-04 12:22:14 +0900

    circles appear on click and have nice tree structure. TODO: improve circles colorscheme, add animations, make calculations happen more responsively

--- geometricSeriesSum.js
+++ geometricSeriesSum.js
@@ -3,6 +3,12 @@ let friendsNumber = 1;
 let layers = 0;
 let sum = 0;
 let circsNum = 0;
+//let pyramid = new Object();
+//let pyramid = [];
+
+//TODO: pick datastruct
+//	make popping animation for the circles as they get created
+//	if sum of circles width > 40vw (figurecontainerwidth) then replace circles with number
 
 document.getElementById("calculateButt").addEventListener("click", e => {
 		onNewNum(friendsNumber, layers);
@@ -29,6 +35,12 @@ function GeometricSeriesSum (a,r,n) {
 		sum += circsNum;
 		console.log(`sum = ${sum}`);
 		AddCircles(circsNum, n);
+		//makedatastruct
+		//~~~~pyramid.layerArray.append(circsNum);
+		//pyramid.[`${n}Layer`] = circsNum;
+		//pyramid = Object.assign(pyramid, {`${n}Layer: circsNum`});
+		//or make a simple array :
+		//pyramid.push(circsNum);
 		n--;	
 	}
 
@@ -49,15 +61,28 @@ function GeometricSeriesSum (a,r,n) {
 }
 function AddCircles (circsNum, layer) {
 	console.log(`added ${circsNum} circles to layer ${layer}`);
+	//change this while loop to be based on the pyramid/array
+	//for each prop in pyramid, add circsNum circles
+	//or
+	//for each index in pyramid pop num off array and add circsnum circles
+
 	while (circsNum > 0 && layer > 0) {
 		let circ = document.createElement("div");
-		circ.className = "circle";
+		circ.className = "circle ";
+		switch (layer) {
+			case 1: circ.className += "layerOne";
+				break;
+			case 2: circ.className += "layerTwo";
+				break;
+			case 3: circ.className += "layerThree";
+		}
 		document.getElementById(`${layer}`).appendChild(circ);
 		circsNum --;
 	}
 }
 function onNewNum (friendsNumber, layers) {
 	console.log("inside onNewNum");
+	//pyramid clear
 	sum = 0;
 	GeometricSeriesSum(baseNumber,friendsNumber,layers);
 	document.getElementById("friendsTextResult").innerHTML = `You have ${sum} friends.`;
